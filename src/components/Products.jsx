194a21b1835a271b2f887656179f6fd45d8778fe
mrkrye <mrkrye@KRAMSEYER>
2024-02-15 12:05:09 -0800

    marks fix to products.jsx

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -13,50 +13,52 @@ import {
   Typography,
 } from "@mui/material";
 
-
 const Products = () => {
   const nav = useNavigate();
   const { data, isLoading } = useGetAllProductsQuery();
   console.log(data);
+
   return (
     <>
-
-    <NavBar/>
+      <div>
+        <NavBar />
+      </div>
       <div className="products">
-
-      <Grid container spacing={2}>
-
-        {isLoading ? (
-          <h1>Loading Products...</h1>
-        ) : (
-          data.map((itm) => (
-            <Grid key={itm.id} item xs={3} md={2.4} lg={2}>
-              <Card sx={{ maxWidth: 345 }}>
-                <CardMedia
-                  sx={{ height: 140 }}
-                  image={itm.imageUrl}
-                  alt={itm.name}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h6" component="div">
-                    {itm.name}
-                  </Typography>
-                  <Typography>{itm.price}</Typography>
-                  <Typography gutterBottom>In Stock: {itm.inStock}</Typography>
-                  <CardActions>
-                    <Button
-                      size="small"
-                      onClick={() => nav(`/products/${itm.id}`)}
-                    >
-                      See Details
-                    </Button>
-                  </CardActions>
-                </CardContent>
-              </Card>
-            </Grid>
-          ))
-        )}
-      </Grid>
+        <Grid container spacing={2}>
+          {isLoading ? (
+            <h1>Loading Products...</h1>
+          ) : (
+            data.map((itm) => (
+              <Grid key={itm.id} item xs={3} md={2.4} lg={2}>
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardMedia
+                    sx={{ height: 140 }}
+                    image={itm.imageUrl}
+                    alt={itm.name}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h6" component="div">
+                      {itm.name}
+                    </Typography>
+                    <Typography>{itm.price}</Typography>
+                    <Typography gutterBottom>
+                      In Stock: {itm.inStock}
+                    </Typography>
+                    <CardActions>
+                      <Button
+                        size="small"
+                        onClick={() => nav(`/products/${itm.id}`)}
+                      >
+                        See Details
+                      </Button>
+                    </CardActions>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))
+          )}
+        </Grid>
+      </div>
     </>
   );
 };
