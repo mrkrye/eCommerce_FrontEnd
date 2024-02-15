3dec89c04aab6c3d842b62dadc1304eb216a968e
mrkrye <mrkrye@KRAMSEYER>
2024-02-15 12:38:10 -0800

    added navbar to all components

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,38 @@
-import NavBar from './NavBar';
-import { Link } from 'react-router-dom';
+import NavBar from "./NavBar";
+import { Link } from "react-router-dom";
 export default function Login() {
   return (
     <>
       <div>
         <NavBar />
+        <form>
+          <h1>Login</h1>
+
+          <div>
+            <input
+              type="email"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Email"
+              name="email"
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              id="exampleInputPassword1"
+              placeholder="Password"
+              name="password"
+            />
+          </div>
+
+          <button type="submit">Login</button>
+
+          <p>
+            No account? <Link to="/register">Sign Up</Link>
+          </p>
+        </form>
       </div>
-      <div>Login</div>
-      <p>
-        No account? <Link to="/register">Sign Up</Link>
-      </p>
     </>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,21 @@
 import { useNavigate } from "react-router-dom";
 
 export default function NavBar() {
-  //   const { token } = useSelector((state) => state.authSlice);
-
   const navigate = useNavigate();
 
   return (
-    <div>
+    <div className="navbar-container">
       <header>
         <nav>
           <a>
             <button onClick={() => navigate("/")}>Home</button>
           </a>
-          <a>  
-             <a>
-            <button onClick={() => navigate("/products")}>Products</button>
-          </a>
+          <a>
+            <a>
+              <button onClick={() => navigate("/products")}>Products</button>
+            </a>
             <button onClick={() => navigate("/login")}>Login</button>
           </a>
-       
         </nav>
       </header>
     </div>
